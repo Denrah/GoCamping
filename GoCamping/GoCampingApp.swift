5cdb8c8150319881db30f9e91f962512abd48423
Denis Sharapov <denisharapov@yandex.ru>
2022-11-12 03:35:41 +0700

    Initial Commit

---
+++ GoCamping/GoCampingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoCampingApp.swift
+//  GoCamping
+//
+//  Created by National Team on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GoCampingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
